5b4b65b8da6554605e45f9fbbee4c65789d4ec2b
unknown <han>
2026-04-22 10:25:14 +0700

    update new

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,9 @@
-﻿
+﻿const soluong=document.getElementsByClassName("sl");
+const dongia=document.getElementsByClassName("dg");
+const thanhtien=document.getElementsByClassName("tt");
+for (let i=0;i<soluong.length;i++)
+{
+    thanhtien[i].innerHTML=soluong[i].innerHTML*dongia[i].innerHTML;
+    tongthanhtoan += parenlnt(thanhtien[i].innerHTML);
+}
+document.getElementById("tongtien").innerHTML=tongthanhtoan;
